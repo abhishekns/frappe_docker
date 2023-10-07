01d771b315d3359a80e8fa06f3cc004ba97163c9
abhishekns <abhishekn.sharma@gmail.com>
2023-10-07 12:04:15 +0530

    added missing devcontainer

--- development/env.mk
+++ development/env.mk
@@ -6,10 +6,10 @@ FRAPPE_BRANCH_VERSION:=version-14
 NVM:=. ${HOME}/.nvm/nvm.sh && nvm 
 
 SKIP_REDIS_CONFIG_GEN=--skip-redis-config-generation
-SITE:=dev.localhost
+SITE:=development.localhost
 MARIADB_PASSWD=123
 ADMIN_PASSWD=admin
 
-APP_NAME:=
-APP_REPO:=
-APP_BRANCH:=
+APP_NAME:=gurukala_internation_app
+APP_REPO:=https://w5mjy2svg3wgzjcjd64euaouoa5gkezldc5gma4fsxa6byirphoq@dev.azure.com/IntlApp/IntGKApp/_git/gurukula_international_app
+APP_BRANCH:=master
--- development/frappe.mk
+++ development/frappe.mk
@@ -1,7 +1,7 @@
 include ../env.mk
 
 site-config:
-	bench set-mariadb-host mariadb
+	bench set-mariadb-host maridadb
 	bench set-redis-cache-host redis://redis-cache:6379
 	bench set-redis-queue-host redis://redis-queue:6379
 	bench set-redis-socketio-host redis://redis-socketio:6379
@@ -9,15 +9,17 @@ site-config:
 	sed -i '/redis/d' ./Procfile
 
 new-site:
-	bench new-site $(SITE) --mariadb-root-password $(MARIADB_PASSWD) --admin-password $(ADMIN_PASSWD) --no-mariadb-socket
+	bench new-site $(SITE) --mariadb-root-password $(MARIADB_PASSWD) --admin-password $(ADMIN_PASSWD) 
 
 dev-config:
 	bench --site $(SITE) set-config developer_mode 1
 	bench --site $(SITE) clear-cache
 
-app:
+get-app:
 	# --branch is optional, use it to point to branch on custom app repository
 	bench get-app --branch $(APP_BRANCH) $(APP_REPO)
+
+install-app:
 	bench --site $(SITE) install-app $(APP_NAME)
 
 	
