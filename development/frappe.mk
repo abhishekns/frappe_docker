d4e68ef77999dfc1a141bf2f6a83dd541a14e5d3
abhishekns <abhishekn.sharma@gmail.com>
2023-10-08 06:41:43 +0530

    Minor fixes to devcontainer and resolving deps while installing app

--- development/frappe.mk
+++ development/frappe.mk
@@ -1,7 +1,7 @@
 include ../env.mk
 
 site-config:
-	bench set-mariadb-host maridadb
+	bench set-mariadb-host mariadb
 	bench set-redis-cache-host redis://redis-cache:6379
 	bench set-redis-queue-host redis://redis-queue:6379
 	bench set-redis-socketio-host redis://redis-socketio:6379
@@ -11,14 +11,17 @@ site-config:
 new-site:
 	bench new-site $(SITE) --mariadb-root-password $(MARIADB_PASSWD) --admin-password $(ADMIN_PASSWD) 
 	bench use $(SITE)
-	
+
+del-site:
+	rm -rf sites/$(SITE)
+
 dev-config:
 	bench --site $(SITE) set-config developer_mode 1
 	bench --site $(SITE) clear-cache
 
 get-app:
 	# --branch is optional, use it to point to branch on custom app repository
-	bench get-app --branch $(APP_BRANCH) $(APP_REPO)
+	bench get-app --branch $(APP_BRANCH) $(APP_REPO) --resolve-deps
 
 install-app:
 	bench --site $(SITE) install-app $(APP_NAME)
